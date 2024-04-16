30c02a915f1a66c3e5102097127ece6075b5c22c
zach <zach@dylibso.com>
2024-04-16 14:17:47 -0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Extism Zig PDK Plugin
+
+
+See more documentation at https://github.com/extism/zig-pdk and
+[join us on Discord](https://extism.org/discord) for more help.
+
---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{
+        // if you're using WASI, change the .os_tag to .wasi
+        .default_target = .{ .abi = .musl, .os_tag = .freestanding, .cpu_arch = .wasm32 },
+    });
+    const pdk_module = b.dependency("extism-pdk", .{ .target = target, .optimize = optimize }).module("extism-pdk");
+    var plugin = b.addExecutable(.{
+        .name = "zig-pdk-template",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    plugin.rdynamic = true;
+    plugin.entry = .disabled; // or add an empty `pub fn main() void {}` to your code
+    plugin.root_module.addImport("extism-pdk", pdk_module);
+
+    b.installArtifact(plugin);
+    const plugin_example_step = b.step("zig-pdk-template", "Build plugin");
+    plugin_example_step.dependOn(b.getInstallStep());
+}
